df86cef223a9a3abfe9feed58e98363fa0e81a2b
Daniil Fefelov <fefelov.danil@gmail.com>
2022-12-26 16:31:31 +0600

    refactor: moved the text to children

--- src/skills/Skill/Skill.tsx
+++ src/skills/Skill/Skill.tsx
@@ -1,39 +1,39 @@
-import React from 'react';
+import React, {FC} from 'react';
 import s from 'skills/Skill/Skill.module.scss'
 import iconSet from "assets/icomoon/selection.json";
 import IcomoonReact from "icomoon-react";
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 import {bgFadeAnimation, fadeBottomAnimation, fadeRightAnimation} from "assets/animation/animation";
 
 type SkillPropsType = {
-    name: string
-    desc: string
-    icon: string
+  children: React.ReactNode
+  name: string
+  icon: string
 }
 
-export const Skill = (props: SkillPropsType) => {
-    return (
-        <motion.div
-            className={s.skill}
-            initial={"hidden"}
-            whileInView={"visible"}
-            viewport={{once: true, amount: 0.3}}>
-            <motion.div
-                className={s.colBg}
-                variants={bgFadeAnimation}>
-            </motion.div>
-            <motion.div
-                className={s.icon}
-                variants={fadeRightAnimation}>
-                <IcomoonReact iconSet={iconSet} color={'#c0f4c0'} icon={props.icon} />
-            </motion.div>
-            <motion.p
-                className={s.skillName}
-                variants={fadeBottomAnimation}>{props.name}</motion.p>
-            <div className={s.desc}>
-                <motion.p variants={fadeBottomAnimation}>{props.desc}</motion.p>
-            </div>
-        </motion.div>
-    );
+export const Skill:FC<SkillPropsType> = ({name, icon, children}) => {
+  return (
+    <motion.div
+      className={s.skill}
+      initial={"hidden"}
+      whileInView={"visible"}
+      viewport={{once: true, amount: 0.3}}>
+      <motion.div
+        className={s.colBg}
+        variants={bgFadeAnimation}>
+      </motion.div>
+      <motion.div
+        className={s.icon}
+        variants={fadeRightAnimation}>
+        <IcomoonReact iconSet={iconSet} color={'#c0f4c0'} icon={icon}/>
+      </motion.div>
+      <motion.p
+        className={s.skillName}
+        variants={fadeBottomAnimation}>{name}</motion.p>
+      <div className={s.desc}>
+        <motion.p variants={fadeBottomAnimation}>{children}</motion.p>
+      </div>
+    </motion.div>
+  );
 };
 
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,32 +2,40 @@ import React from 'react';
 import s from 'skills/Skills.module.scss'
 import {Skill} from "./Skill/Skill";
 import blockBackground from 'assets/images/fon-2.jpg'
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 import {textBgAnimation} from "assets/animation/animation";
 
 export const Skills = () => {
-    return (
-        <div
-            className={s.skillsBlock}
-            style = {{backgroundImage: `url('${blockBackground}')`}}
-            id={'anchorSkills'}>
-            <div className={"container"}>
-                <motion.h2
-                    initial={"hidden"}
-                    whileInView={"visible"}
-                    viewport={{once: true, amount: 0.9}}>
-                    <span className="titleBg">НАВЫКИ
-                        <motion.span className="titleBgIn" variants={textBgAnimation}></motion.span>
-                    </span>
-                </motion.h2>
-                <div className={s.skills}>
-                    <Skill name={"JS"} icon={'js'} desc={"Много времени уделил изучению js. Применял знания на практике. Создавал сайты с калькуляторами и различными интерактивными элементами. Интересен этот язык программирования."}/>
-                    <Skill name={"React"} icon={'react-logo'} desc={"React + Redux - это две технологии которые для меня наиболее интересны. Создал несколько pet проектов на React. В которых использовал redux, работал с сервером при помощи thunk и axios, писал unit тесты, использовал storybook, применял material ui. Больше всего хочу работать именно с React."}/>
-                    <Skill name={"HTML"} icon={'html'} desc={"Более 6 лет занимался созданием сайтов, работал напрямую с заказчиками. Делал на WordPress и October CMS. Хорошо знаю HTML, однако хочу работать в первую очерель с js и React. Но уверен, что опыт который получил будет полезен."}/>
-                    <Skill name={"CSS"} icon={'css3'} desc={"Большой опыт в написании css стилей. Все сайты, которые создавал, делал адаптивными. Знаю препроцессоры в своих pet пароектах использовал scss. Хотя в работе при создании сайтов писал все на чистом css."}/>
-                </div>
-            </div>
+  return (
+    <div
+      className={s.skillsBlock}
+      style={{backgroundImage: `url('${blockBackground}')`}}
+      id={'anchorSkills'}>
+      <div className={"container"}>
+        <motion.h2
+          initial={"hidden"}
+          whileInView={"visible"}
+          viewport={{once: true, amount: 0.9}}>
+            <span className="titleBg">НАВЫКИ
+                <motion.span className="titleBgIn" variants={textBgAnimation}></motion.span>
+            </span>
+        </motion.h2>
+        <div className={s.skills}>
+          <Skill name={"JS"} icon={'js'}>
+            <span>Много времени уделил изучению js. Применял знания на практике. Создавал сайты с калькуляторами и различными интерактивными элементами. Интересен этот язык программирования.</span>
+          </Skill>
+          <Skill name={"React"} icon={'react-logo'}>
+            <span>React + Redux - это две технологии которые для меня наиболее интересны. Создал несколько pet проектов на React. В которых использовал redux, работал с сервером при помощи thunk и axios, писал unit тесты, использовал storybook, применял material ui. Больше всего хочу работать именно с React.</span>
+          </Skill>
+          <Skill name={"HTML"} icon={'html'}>
+            <span>Более 6 лет занимался созданием сайтов, работал напрямую с заказчиками. Делал на WordPress и October CMS. Хорошо знаю HTML, однако хочу работать в первую очерель с js и React. Но уверен, что опыт который получил будет полезен.</span>
+          </Skill>
+          <Skill name={"CSS"} icon={'css3'}>
+            <span>Большой опыт в написании css стилей. Все сайты, которые создавал, делал адаптивными. Знаю препроцессоры в своих pet пароектах использовал scss. Хотя в работе при создании сайтов писал все на чистом css.</span>
+          </Skill>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
